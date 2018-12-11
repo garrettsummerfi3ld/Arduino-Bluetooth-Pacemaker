5e11ae70268980872ed9ddb8d54ce953e690ad45
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2018-12-11 15:28:07 -0600

    Adding some imports for VSCode and local variables

--- Bluetooth_test.ino
+++ Bluetooth_test.ino
@@ -2,7 +2,7 @@
  * This is a very early prototype with sample code
  * this will change later soon enough and will actually function once this is done
  */
-char data = 0;            //Variable for storing received data
+char shockData = 0;            //Variable for storing received data
 void setup()
 {
     Serial.begin(9600);   //Sets the baud for serial data transmission                               
@@ -12,12 +12,12 @@ void loop()
 {
    if(Serial.available() > 0)      // Send data only when you receive data:
    {
-      data = Serial.read();        //Read the incoming data & store into data
-      Serial.print(data);          //Print Value inside data in Serial monitor
+      shockData = Serial.read();        //Read the incoming data & store into data
+      Serial.print(shockData);          //Print Value inside data in Serial monitor
       Serial.print("\n");        
-      if(data == '1')              // Checks whether value of data is equal to 1
+      if(shockData == '1')              // Checks whether value of data is equal to 1
          digitalWrite(13, HIGH);   //If value is 1 then LED turns ON
-      else if(data == '0')         //  Checks whether value of data is equal to 0
+      else if(shockData == '0')         //  Checks whether value of data is equal to 0
          digitalWrite(13, LOW);    //If value is 0 then LED turns OFF
    }
 }
